55f6ffad48d89a98c9116ff941ab44fef0f95857
Grzechosz <grzechosz125@gmail.com>
2023-10-11 20:42:13 +0200

    camera screen implementation completed

--- lib/Scanner.dart
+++ lib/Scanner.dart
@@ -1,106 +1,112 @@
 import 'dart:io';
 
 import 'package:camera/camera.dart';
+import 'package:consciousconsumer/TesseractTextRecognizer.dart';
 import 'package:flutter/material.dart';
 
 class ScannerScreen extends StatefulWidget {
-  const ScannerScreen({super.key,
-  required this.camera,
-});
+  const ScannerScreen({
+    super.key,
+    required this.camera,
+  });
 
-final CameraDescription camera;
+  final CameraDescription camera;
 
-@override
-ScannerScreenState createState() => ScannerScreenState();
+  @override
+  ScannerScreenState createState() => ScannerScreenState();
 }
 
 class ScannerScreenState extends State<ScannerScreen> {
   late CameraController _controller;
+  late TesseractTextRecognizer _tesseractTextRecognizer;
   late Future<void> _initializeControllerFuture;
 
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // Get a specific camera from the list of available cameras.
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.high,
+    );
 
-@override
-void initState() {
-  super.initState();
-  // To display the current output from the Camera,
-  // create a CameraController.
-  _controller = CameraController(
-    // Get a specific camera from the list of available cameras.
-    widget.camera,
-    // Define the resolution to use.
-    ResolutionPreset.medium,
-  );
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+    _tesseractTextRecognizer = TesseractTextRecognizer();
+  }
 
-  // Next, initialize the controller. This returns a Future.
-  _initializeControllerFuture = _controller.initialize();
-}
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
 
-@override
-void dispose() {
-  // Dispose of the controller when the widget is disposed.
-  _controller.dispose();
-  super.dispose();
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Take a picture')),
+      // You must wait until the controller is initialized before displaying the
+      // camera preview. Use a FutureBuilder to display a loading spinner until the
+      // controller has finished initializing.
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        // Provide an onPressed callback.
+        onPressed: () async {
+          // Take the Picture in a try / catch block. If anything goes wrong,
+          // catch the error.
+          try {
+            // Ensure that the camera is initialized.
+            await _initializeControllerFuture;
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(title: const Text('Take a picture')),
-    // You must wait until the controller is initialized before displaying the
-    // camera preview. Use a FutureBuilder to display a loading spinner until the
-    // controller has finished initializing.
-    body: FutureBuilder<void>(
-      future: _initializeControllerFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          // If the Future is complete, display the preview.
-          return CameraPreview(_controller);
-        } else {
-          // Otherwise, display a loading indicator.
-          return const Center(child: CircularProgressIndicator());
-        }
-      },
-    ),
-    floatingActionButton: FloatingActionButton(
-      // Provide an onPressed callback.
-      onPressed: () async {
-        // Take the Picture in a try / catch block. If anything goes wrong,
-        // catch the error.
-        try {
-          // Ensure that the camera is initialized.
-          await _initializeControllerFuture;
+            // Attempt to take a picture and get the file `image`
+            // where it was saved.
+            final image = await _controller.takePicture();
+            final stringDesc = await _tesseractTextRecognizer.processImage(image.path);
 
-          // Attempt to take a picture and get the file `image`
-          // where it was saved.
-          final image = await _controller.takePicture();
+            if (!mounted) return;
 
-          if (!mounted) return;
-
-          // If the picture was taken, display it on a new screen.
-          await Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => DisplayPictureScreen(
-                // Pass the automatically generated path to
-                // the DisplayPictureScreen widget.
-                imagePath: image.path,
+            // If the picture was taken, display it on a new screen.
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => DisplayPictureScreen(
+                  // Pass the automatically generated path to
+                  // the DisplayPictureScreen widget.
+                  imagePath: image.path,
+                  image_discription: stringDesc,
+                ),
               ),
-            ),
-          );
-        } catch (e) {
-          // If an error occurs, log the error to the console.
-          print(e);
-        }
-      },
-      child: const Icon(Icons.camera_alt),
-    ),
-  );
-}
+            );
+          } catch (e) {
+            // If an error occurs, log the error to the console.
+            print(e);
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
+  }
 }
 
 class DisplayPictureScreen extends StatelessWidget {
   final String imagePath;
+  final String image_discription;
 
-  const DisplayPictureScreen({super.key, required this.imagePath});
+  const DisplayPictureScreen({super.key, required this.imagePath, required this.image_discription});
 
   @override
   Widget build(BuildContext context) {
@@ -108,7 +114,11 @@ class DisplayPictureScreen extends StatelessWidget {
       appBar: AppBar(title: const Text('Display the Picture')),
       // The image is stored as a file on the device. Use the `Image.file`
       // constructor with the given path to display the image.
-      body: Image.file(File(imagePath)),
+      body: Container(
+        child: Column(
+          children: [Image.file(File(imagePath)), Text(image_discription)],
+        ),
+      ),
     );
   }
 }
